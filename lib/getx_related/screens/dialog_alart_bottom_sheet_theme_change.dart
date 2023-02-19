05e94b54009218982087a68fdb5121014471adee
Shahidullah191 <shahidullah.cse.gub@gmail.com>
2023-02-19 12:14:36 +0600

    first commit

---
+++ lib/getx_related/screens/dialog_alart_bottom_sheet_theme_change.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AlartBottomTheme extends StatefulWidget {
+  const AlartBottomTheme({Key? key}) : super(key: key);
+
+  @override
+  State<AlartBottomTheme> createState() => _AlartBottomThemeState();
+}
+
+class _AlartBottomThemeState extends State<AlartBottomTheme> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
